d015f12d0a43c8f33202a6d4e26d16abbcffa5f6
fallengold <Fallengold2004@outlook.com>
2025-05-03 13:55:04 +0800

    finish Fundamental matrix

--- homework/helper.py
+++ homework/helper.py
@@ -2,10 +2,12 @@
 Homework2.
 Helper functions.
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize
-import submission as sub
+import homework.submission as sub
+
 
 def _epipoles(E):
     U, S, V = np.linalg.svd(E)
@@ -14,16 +16,20 @@ def _epipoles(E):
     e2 = V[-1, :]
     return e1, e2
 
+
 def displayEpipolarF(I1, I2, F):
     e1, e2 = _epipoles(F)
 
     sy, sx, _ = I2.shape
 
-    f, [ax1, ax2] = plt.subplots(1, 2, figsize=(12, 9)) ax1.imshow(I1)
-    ax1.set_title('Select a point in this image')
+    f, [ax1, ax2] = plt.subplots(1, 2, figsize=(12, 9))
+    ax1.imshow(I1)
+    ax1.set_title("Select a point in this image")
     ax1.set_axis_off()
     ax2.imshow(I2)
-    ax2.set_title('Verify that the corresponding point \n is on the epipolar line in this image')
+    ax2.set_title(
+        "Verify that the corresponding point \n is on the epipolar line in this image"
+    )
     ax2.set_axis_off()
 
     while True:
@@ -34,25 +40,25 @@ def displayEpipolarF(I1, I2, F):
         yc = y
         v = np.array([xc, yc, 1])
         l = F.dot(v)
-        s = np.sqrt(l[0]**2+l[1]**2)
+        s = np.sqrt(l[0] ** 2 + l[1] ** 2)
 
-        if s==0:
-            error('Zero line vector in displayEpipolar')
+        if s == 0:
+            error("Zero line vector in displayEpipolar")
 
-        l = l/s
+        l = l / s
 
         if l[0] != 0:
-            ye = sy-1
+            ye = sy - 1
             ys = 0
-            xe = -(l[1] * ye + l[2])/l[0]
-            xs = -(l[1] * ys + l[2])/l[0]
+            xe = -(l[1] * ye + l[2]) / l[0]
+            xs = -(l[1] * ys + l[2]) / l[0]
         else:
-            xe = sx-1
+            xe = sx - 1
             xs = 0
-            ye = -(l[0] * xe + l[2])/l[1]
-            ys = -(l[0] * xs + l[2])/l[1]
+            ye = -(l[0] * xe + l[2]) / l[1]
+            ys = -(l[0] * xs + l[2]) / l[1]
 
-        ax1.plot(x, y, '*', markersize=6, linewidth=2)
+        ax1.plot(x, y, "*", markersize=6, linewidth=2)
         ax2.plot([xs, xe], [ys, ye], linewidth=2)
         plt.draw()
 
@@ -62,6 +68,7 @@ def _singularize(F):
     S[-1] = 0
     F = U.dot(np.diag(S).dot(V))
     return F
+
 
 def _objective_F(f, pts1, pts2):
     F = _singularize(f.reshape([3, 3]))
@@ -73,33 +80,47 @@ def _objective_F(f, pts1, pts2):
 
     r = 0
     for fp1, fp2, hp2 in zip(Fp1.T, FTp2.T, hpts2):
-        r += (hp2.dot(fp1))**2 * (1/(fp1[0]**2 + fp1[1]**2) + 1/(fp2[0]**2 + fp2[1]**2))
+        r += (hp2.dot(fp1)) ** 2 * (
+            1 / (fp1[0] ** 2 + fp1[1] ** 2) + 1 / (fp2[0] ** 2 + fp2[1] ** 2)
+        )
     return r
+
 
 def refineF(F, pts1, pts2):
     f = scipy.optimize.fmin_powell(
-        lambda x: _objective_F(x, pts1, pts2), F.reshape([-1]),
+        lambda x: _objective_F(x, pts1, pts2),
+        F.reshape([-1]),
         maxiter=100000,
-        maxfun=10000
+        maxfun=10000,
     )
     return _singularize(f.reshape([3, 3]))
 
-def camera2(E):
-    U,S,V = np.linalg.svd(E)
-    m = S[:2].mean()
-    E = U.dot(np.array([[m,0,0], [0,m,0], [0,0,0]])).dot(V)
-    U,S,V = np.linalg.svd(E)
-    W = np.array([[0,-1,0], [1,0,0], [0,0,1]])
 
-    if np.linalg.det(U.dot(W).dot(V))<0:
+def camera2(E):
+    U, S, V = np.linalg.svd(E)
+    m = S[:2].mean()
+    E = U.dot(np.array([[m, 0, 0], [0, m, 0], [0, 0, 0]])).dot(V)
+    U, S, V = np.linalg.svd(E)
+    W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+
+    if np.linalg.det(U.dot(W).dot(V)) < 0:
         W = -W
 
-    M2s = np.zeros([3,4,4])
-    M2s[:,:,0] = np.concatenate([U.dot(W).dot(V), U[:,2].reshape([-1, 1])/abs(U[:,2]).max()], axis=1)
-    M2s[:,:,1] = np.concatenate([U.dot(W).dot(V), -U[:,2].reshape([-1, 1])/abs(U[:,2]).max()], axis=1)
-    M2s[:,:,2] = np.concatenate([U.dot(W.T).dot(V), U[:,2].reshape([-1, 1])/abs(U[:,2]).max()], axis=1)
-    M2s[:,:,3] = np.concatenate([U.dot(W.T).dot(V), -U[:,2].reshape([-1, 1])/abs(U[:,2]).max()], axis=1)
+    M2s = np.zeros([3, 4, 4])
+    M2s[:, :, 0] = np.concatenate(
+        [U.dot(W).dot(V), U[:, 2].reshape([-1, 1]) / abs(U[:, 2]).max()], axis=1
+    )
+    M2s[:, :, 1] = np.concatenate(
+        [U.dot(W).dot(V), -U[:, 2].reshape([-1, 1]) / abs(U[:, 2]).max()], axis=1
+    )
+    M2s[:, :, 2] = np.concatenate(
+        [U.dot(W.T).dot(V), U[:, 2].reshape([-1, 1]) / abs(U[:, 2]).max()], axis=1
+    )
+    M2s[:, :, 3] = np.concatenate(
+        [U.dot(W.T).dot(V), -U[:, 2].reshape([-1, 1]) / abs(U[:, 2]).max()], axis=1
+    )
     return M2s
+
 
 def epipolarMatchGUI(I1, I2, F):
     e1, e2 = _epipoles(F)
@@ -108,10 +129,12 @@ def epipolarMatchGUI(I1, I2, F):
 
     f, [ax1, ax2] = plt.subplots(1, 2, figsize=(12, 9))
     ax1.imshow(I1)
-    ax1.set_title('Select a point in this image')
+    ax1.set_title("Select a point in this image")
     ax1.set_axis_off()
     ax2.imshow(I2)
-    ax2.set_title('Verify that the corresponding point \n is on the epipolar line in this image')
+    ax2.set_title(
+        "Verify that the corresponding point \n is on the epipolar line in this image"
+    )
     ax2.set_axis_off()
 
     while True:
@@ -122,34 +145,34 @@ def epipolarMatchGUI(I1, I2, F):
         yc = int(y)
         v = np.array([xc, yc, 1])
         l = F.dot(v)
-        s = np.sqrt(l[0]**2+l[1]**2)
+        s = np.sqrt(l[0] ** 2 + l[1] ** 2)
 
-        if s==0:
-            error('Zero line vector in displayEpipolar')
+        if s == 0:
+            error("Zero line vector in displayEpipolar")
 
-        l = l/s
+        l = l / s
 
         if l[0] != 0:
-            ye = sy-1
+            ye = sy - 1
             ys = 0
-            xe = -(l[1] * ye + l[2])/l[0]
-            xs = -(l[1] * ys + l[2])/l[0]
+            xe = -(l[1] * ye + l[2]) / l[0]
+            xs = -(l[1] * ys + l[2]) / l[0]
         else:
-            xe = sx-1
+            xe = sx - 1
             xs = 0
-            ye = -(l[0] * xe + l[2])/l[1]
-            ys = -(l[0] * xs + l[2])/l[1]
+            ye = -(l[0] * xe + l[2]) / l[1]
+            ys = -(l[0] * xs + l[2]) / l[1]
 
-        ax1.plot(x, y, '*', markersize=6, linewidth=2)
+        ax1.plot(x, y, "*", markersize=6, linewidth=2)
         ax2.plot([xs, xe], [ys, ye], linewidth=2)
 
         # draw points
         x2, y2 = sub.epipolarCorrespondence(I1, I2, F, xc, yc)
-        ax2.plot(x2, y2, 'ro', markersize=8, linewidth=2)
+        ax2.plot(x2, y2, "ro", markersize=8, linewidth=2)
         plt.draw()
 
+
 def getAbsoluteScale(pos_frame_prev, pose_frame_curr):
-    
     """
     Estimation of scale for multiplying translation vectors
     :return: Scalar multiplier
@@ -159,7 +182,6 @@ def getAbsoluteScale(pos_frame_prev, pose_frame_curr):
     x_prev = pos_frame_prev[0]
     y_prev = pos_frame_prev[1]
     z_prev = pos_frame_prev[2]
-
 
     x = pose_frame_curr[0]
     y = pose_frame_curr[1]
--- homework/submission.py
+++ homework/submission.py
@@ -4,31 +4,146 @@ Replace 'pass' by your implementation.
 """
 
 # Insert your package here
+import cv2
+import numpy as np
+from homework.helper import refineF
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
 
-'''
+
+def find_matched_points(im1, im2, output_image_path="matches.jpg"):
+    """
+    Args: im1: First input image
+          im2: Second input image
+          output_image_path: Path to save the visualization of matches
+
+    Returns:
+        pts1: Nx2 array of matched points in the first image
+        pts2: Nx2 array of matched points in the second image
+    """
+    # Ensure images are grayscale for SIFT
+    if len(im1.shape) == 3:
+        gray1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    else:
+        gray1 = im1
+    if len(im2.shape) == 3:
+        gray2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+    else:
+        gray2 = im2
+
+    gray1 = cv2.equalizeHist(gray1)
+    gray2 = cv2.equalizeHist(gray2)
+
+    # gray1 = im1
+    # gray2 = im2
+
+    # Initialize SIFT detector
+    sift = cv2.SIFT_create(contrastThreshold=0.04, edgeThreshold=10)
+
+    keypoints1, descriptors1 = sift.detectAndCompute(gray1, None)
+    keypoints2, descriptors2 = sift.detectAndCompute(gray2, None)
+
+    # bf matcher
+    bf = cv2.BFMatcher(cv2.NORM_L2)
+    matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+    matches = [m for m, n in matches if m.distance < 0.75 * n.distance]
+    matches = sorted(matches, key=lambda x: x.distance)
+    pts1 = np.float32([keypoints1[m.queryIdx].pt for m in matches])
+    pts2 = np.float32([keypoints2[m.trainIdx].pt for m in matches])
+
+    print(f"Keypoints in im1: {len(keypoints1)}")
+    print(f"Keypoints in im2: {len(keypoints2)}")
+    print(f"Initial matches: {len(matches)}")
+
+    pts1 = np.float32([keypoints1[m.queryIdx].pt for m in matches])
+    pts2 = np.float32([keypoints2[m.trainIdx].pt for m in matches])
+
+    distances = [m.distance for m in matches]
+    print(
+        f"Match distances: min={min(distances)}, max={max(distances)}, mean={np.mean(distances)}"
+    )
+
+    model, inliers = ransac(
+        (pts1, pts2),
+        FundamentalMatrixTransform,
+        min_samples=8,
+        residual_threshold=1,
+        max_trials=10000,
+    )
+
+    if inliers is not None:
+
+        pts1 = pts1[inliers]
+        pts2 = pts2[inliers]
+
+    matches = [m for i, m in enumerate(matches) if inliers[i]]
+    print(f"RANSAC matches: {len(matches)}")
+    top_matches = matches[:100]
+    match_img = cv2.drawMatches(
+        im1,
+        keypoints1,
+        im2,
+        keypoints2,
+        top_matches,
+        None,
+        flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+    )
+
+    cv2.imwrite(output_image_path, match_img)
+    return pts1, pts2, model
+
+
+"""
 Q2.2.2: Eight Point Algorithm
     Input:  pts1, Nx2 Matrix
             pts2, Nx2 Matrix
             M, a scalar parameter computed as max (imwidth, imheight)
-    Output: F, the fundamental matrix
-'''
+    Output: the fundamental matrix
+"""
+
+
 def eightpoint(pts1, pts2, M):
     # Replace pass by your implementation
-    pass
+    T = np.array([[1 / M, 0.0, 0.0], [0.0, 1 / M, 0.0], [0.0, 0.0, 1.0]])
+    N = pts1.shape[0]
+
+    ones = np.ones((N, 1))
+    pts1_h = np.hstack([pts1 / M, ones])
+    pts2_h = np.hstack([pts2 / M, ones])  # N x 3
+
+    # Construct A matrix
+    A = np.zeros((N, 9))
+    for i in range(N):
+        x1, y1, _ = pts1_h[i]
+        x2, y2, _ = pts2_h[i]
+        A[i] = [x2 * x1, x2 * y1, x2, y2 * x1, y2 * y1, y2, x1, y1, 1]
+
+    # Solve for the SVD
+    _, _, V = np.linalg.svd(A)
+    F_normalized = V[-1].reshape(3, 3)
+    U, S, Vt = np.linalg.svd(F_normalized)
+    S[-1] = 0
+    F_normalized = U @ np.diag(S) @ Vt
+    # F_normalized = refineF(F_normalized, pts1 / M, pts2 / M)
+    F = T.T @ F_normalized @ T
+    return F
 
 
-'''
+"""
 Q2.3.1: Compute the essential matrix E.
     Input:  F, fundamental matrix
             K1, internal camera calibration matrix of camera 1
             K2, internal camera calibration matrix of camera 2
     Output: E, the essential matrix
-'''
+"""
+
+
 def essentialMatrix(F, K1, K2):
     # Replace pass by your implementation
     pass
 
-'''
+
+"""
 Q2.3.2: Triangulate a set of 2D coordinates in the image to a set of 3D points.
     Input:  C1, the 3x4 camera matrix
             pts1, the Nx2 matrix with the 2D image coordinates per row
@@ -36,13 +151,15 @@ Q2.3.2: Triangulate a set of 2D coordinates in the image to a set of 3D points.
             pts2, the Nx2 matrix with the 2D image coordinates per row
     Output: P, the Nx3 matrix with the corresponding 3D points per row
             err, the reprojection error.
-'''
+"""
+
+
 def triangulate(C1, pts1, C2, pts2):
     # Replace pass by your implementation
     pass
 
 
-'''
+"""
 Q2.4.1: 3D visualization of the temple images.
     Input:  im1, the first image
             im2, the second image
@@ -52,12 +169,15 @@ Q2.4.1: 3D visualization of the temple images.
     Output: x2, x-coordinates of the pixel on im2
             y2, y-coordinates of the pixel on im2
 
-'''
+"""
+
+
 def epipolarCorrespondence(im1, im2, F, x1, y1):
     # Replace pass by your implementation
     pass
 
-'''
+
+"""
 Q3.1: Decomposition of the essential matrix to rotation and translation.
     Input:  im1, the first image
             im2, the second image
@@ -66,23 +186,24 @@ Q3.1: Decomposition of the essential matrix to rotation and translation.
     Output: R, rotation
             r, translation
 
-'''
+"""
+
+
 def essentialDecomposition(im1, im2, k1, k2):
     # Replace pass by your implementation
     pass
 
 
-'''
+"""
 Q3.2: Implement a monocular visual odometry.
     Input:  datafolder, the folder of the provided monocular video sequence
             GT_pose, the provided ground-truth (GT) pose for each frame
             plot=True, draw the estimated and the GT camera trajectories in the same plot
     Output: trajectory, the estimated camera trajectory (with scale aligned)        
 
-'''
+"""
+
+
 def visualOdometry(datafolder, GT_Pose, plot=True):
     # Replace pass by your implementation
     pass
-
-
-
